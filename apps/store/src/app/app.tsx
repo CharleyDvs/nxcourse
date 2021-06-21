6d3527b5cf4a34b7a7d34e93b556d9cbee10a465
CharleyDvs <ing.carlosjaime@outlook.com>
2021-06-21 13:34:17 -0500

    Added new card component to present the data from the fake API

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,9 +1,45 @@
 import styles from './app.module.scss';
 
+import { getAllGames } from '../fake-api';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import { CardMedia } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
 export function App() {
   return (
-    <div className={styles.app}>
-      <h1>Hi</h1>
+    <div className="container">
+      <div className="games-layout">
+        {getAllGames().map((x) => (
+          <Card key={x.id} className="game-card">
+            <CardActionArea>
+              <CardMedia
+                className="game-card-media"
+                image={x.image}
+                title={x.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {x.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {x.description}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  className="game-rating"
+                >
+                  <strong>Rating:</strong> {x.rating}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
